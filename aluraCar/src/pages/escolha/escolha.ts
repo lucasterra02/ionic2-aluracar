1654cf7886bac23de4728d91e25a0198f644cd0c
lucasterra02 <lucasterra02@gmail.com>
2017-05-31 22:51:24 -0300

    criacao de toggle e novas classes

--- aluraCar/src/pages/escolha/escolha.ts
+++ aluraCar/src/pages/escolha/escolha.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavParams } from 'ionic-angular';
+import { Acessorio } from '../../domain/carro/acessorio';
+import { Carro } from '../../domain/carro/carro';
 
 @Component({
     templateUrl: 'escolha.html'
@@ -7,11 +9,29 @@ import { NavParams } from 'ionic-angular';
 
 export class EscolhaPage {
 
-    public carro;
+    public carro: Carro;
+    public acessorios: Acessorio[];
+    private _precoTotal: number;
+
     constructor(public navParams: NavParams) {
 
         this.carro = this.navParams.get('carroSelecionado');
-        console.log(this.carro.nome);
+        this._precoTotal = this.carro.preco;
+        this.acessorios = [
+            new Acessorio('Freio ABS', 800),
+            new Acessorio('Ar-condicionado', 1000),
+            new Acessorio('MP3 Player', 500)
+        ];
+    }
+
+    get precoTotal() {
+        return this._precoTotal;
+    }
+
+    atualizaTotal(ligado: boolean, acessorio: Acessorio) {
+        ligado ?
+            this._precoTotal+=acessorio.preco :
+            this._precoTotal-=acessorio.preco;
     }
 
 }
